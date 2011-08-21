480c2708c2ab4cc2a9b077f814df05b3fd55d7c6
Ankit Daftery <ankitdaf@gmail.com>
2011-08-22 00:56:37 +0530

    Added support to allow nesting upto one directory

--- phototime.py
+++ phototime.py
@@ -4,12 +4,29 @@ import subprocess
 
 path = sys.argv[1]
 ftype = "/*." + sys.argv[2]	# I can't believe I was too lazy to add this line before ;)
-# os.listdir(path) Add this for nested directories later + 
-filelist = subprocess.Popen("ls " + path + ftype , shell=True,stdout=subprocess.PIPE, cwd=None).stdout.read().strip('\n').split('\n')
-for i in filelist:
-	statinfo = os.stat(i)
-	newname= time.strftime("%Y-%m-%d %H.%M:%S", time.localtime(statinfo.st_mtime)) + ".%s"%(sys.argv[2])
-	os.renames(i,path+"/"+newname)
+dirlist = []
+dirlist.append('.')		# This is to ensure that the main cause is not lost in the melee
 
+# A loop to get all the directories in the current directory
+# Works only for 1 level right now. No whitespaces in directory name please
+# Should have been recursive, but I am too sleepy right now
+# Will do it later
 
+for i in os.listdir(path):
+	if(os.path.isfile(i) != 1):
+		dirlist.append("./" + i)
 
+# Unleash the hounds on those wretched filepaths, I say
+
+for i in dirlist:
+	# os.listdir(path) Add this for nested directories later + 
+	filelist = subprocess.Popen("ls " + i + ftype , shell=True,stdout=subprocess.PIPE, cwd=None).stdout.read().strip('\n').split('\n')
+	for j in filelist:
+		try:
+			statinfo = os.stat(j)
+		except OSError:
+			continue
+		newname= time.strftime("%Y-%m-%d %H.%M:%S", time.localtime(statinfo.st_mtime)) + ".%s"%(sys.argv[2])
+		os.renames(j,i+"/"+newname)
+
+# And someone once said "What's in a name" .. Hah, in your face !!
