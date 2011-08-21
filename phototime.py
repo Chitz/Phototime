9a2915c891644d978a65eb7cef2b3764570c371b
Ankit Daftery <ankitdaf@gmail.com>
2011-08-21 23:51:10 +0530

    Made the code modular. Supports all formats now. Check README.

--- phototime.py
+++ phototime.py
@@ -3,11 +3,12 @@ import os, time
 import subprocess
 
 path = sys.argv[1]
+ftype = "/*." + sys.argv[2]
 # os.listdir(path) Add this for nested directories later + 
-filelist = subprocess.Popen("ls " + path + "/*.JPG" , shell=True,stdout=subprocess.PIPE, cwd=None).stdout.read().strip('\n').split('\n')
+filelist = subprocess.Popen("ls " + path + ftype , shell=True,stdout=subprocess.PIPE, cwd=None).stdout.read().strip('\n').split('\n')
 for i in filelist:
 	statinfo = os.stat(i)
-	newname= time.strftime("%Y-%m-%d %H.%M:%S", time.localtime(statinfo.st_mtime)) + ".jpg"
+	newname= time.strftime("%Y-%m-%d %H.%M:%S", time.localtime(statinfo.st_mtime)) + ".%s"%(sys.argv[2])
 	os.renames(i,path+"/"+newname)
 
 
