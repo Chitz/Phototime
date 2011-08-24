076c3a9e0e5b0096280fff5c1a1eaea120c57138
Chitesh Tewani <chiteshtewani@gmail.com>
2011-08-24 08:21:10 +0530

    Added Recursion, Final Code..

--- phototime.py
+++ phototime.py
@@ -6,27 +6,18 @@ import sys
 pathname = sys.argv[1]
 locationName = sys.argv[2]
 file_extension = sys.argv[3]
-def change(pathname):
+def rename(pathname):
     for filename in glob.glob(os.path.join(pathname,'*.%s'%file_extension)):
-        createdTime_Epoch = os.path.getmtime(filename)
-        dateNtime = time.localtime(createdTime_Epoch)
-        Day = dateNtime.tm_mday
-        Month = dateNtime.tm_mon
-        Year = dateNtime.tm_year
-        Hour = dateNtime.tm_hour
-        Min = dateNtime.tm_min
-        Sec = dateNtime.tm_sec
-        CreatedTime = str(Hour)+":"+str(Min)+":"+str(Sec)
-        CreatedDate = str(Day) + '-' +str(Month) + '-' +str(Year)
-        TimeStamp = CreatedDate+"_"+CreatedTime
+        statinfo = os.stat(filename)
+        TimeStamp= time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime(statinfo.st_mtime))
         path = pathname + '/'
         os.rename(filename,path+locationName+"_"+TimeStamp+'.%s'%file_extension)
 
-def get_file(pathname):
+def traverse(pathname):
     for f in os.listdir(pathname):
         if os.path.isdir(pathname+'/%s'%f) == True:
-            get_file(pathname+'/%s'%f)
+            traverse(pathname+'/%s'%f)
         else:
-            change(pathname)
+            rename(pathname)
 
-get_file(pathname)
+traverse(pathname)
